ab60121eda35c545099fadc96fbca1ab875aafca
Jasm Sison <jasm.sison@gmail.com>
2020-06-02 12:25:54 +0200

    rename

--- test/index.dart
+++ test/index.dart
@@ -1,9 +1,9 @@
-import "test/ast_exhaustive_field_test.dart" as ast_exhaustive_field_test;
-import "test/ast_layout_parser_test.dart" as ast_layout_parser_test;
-import "test/fn_ptr_parser_test.dart" as fn_ptr_parser_test;
-import "test/irgen_layout_parser_test.dart" as irgen_layout_parser_test;
-import "test/layout_parser_transition_test.dart" as layout_parser_transition_test;
-import "test/ptr_syntax_test.dart" as ptr_syntax_test;
+import "ast_exhaustive_field_test.dart" as ast_exhaustive_field_test;
+import "ast_layout_parser_test.dart" as ast_layout_parser_test;
+import "fn_ptr_parser_test.dart" as fn_ptr_parser_test;
+import "irgen_layout_parser_test.dart" as irgen_layout_parser_test;
+import "layout_parser_transition_test.dart" as layout_parser_transition_test;
+import "ptr_syntax_test.dart" as ptr_syntax_test;
 
 void main() {
   ast_exhaustive_field_test.main();
